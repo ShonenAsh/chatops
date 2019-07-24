244369d0639c71a0a384ae5c218663542ef36fef
Ashish Annappa Magadum <shonenash@gmail.com>
2019-07-24 20:39:10 +0530

    book-route

--- src/app.js
+++ src/app.js
@@ -5,11 +5,11 @@ const app = express();
 (async function main() {
 
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false});
+    const url = 'https://libgen.is';
 
     app.get('/', async (req, res) => {
 
-      const url = 'https://libgen.is';
       const page = await browser.newPage();
       await page.goto(url); //, {waitUntil: 'networkidle0'});
       const fic = await page.$('a[href="/foreignfiction/index.php"]');
@@ -22,8 +22,34 @@ const app = express();
         
     });
 
+    // "/bookname/genre"
+    app.get('/:bookname/:genre', async (req,res) => {
+      let bookname=req.params.bookname;
+      let genre = req.params.genre;
+
+      res.send({Name: bookname, Genre: genre});
+
+    });
+
     app.get('/:bookname', async (req,res) => {
-        res.send(req.params.bookname);
+      let bookname=req.params.bookname;
+
+      const page = await browser.newPage();
+      await page.goto(url, {waitUntil: 'networkidle0'});
+      const selector = '#searchform';
+      await page.waitForSelector(selector);
+      await page.type(selector,bookname);
+
+      // const input = await page.$('input[name="req"]');
+      // input.value = await bookname;
+
+      const searchButton = await page.$('input[type="submit"]');
+      await searchButton.click();
+      await page.waitForNavigation();
+
+      const html = await page.content();
+      res.send(html);
+      console.log(html);
     });
 
   } catch (err) {
@@ -32,8 +58,8 @@ const app = express();
 
     // Listening to requests
   const server = app.listen(process.env.PORT || 8080, err => {
-  if (err) {
-    return console.error(err);
+    if (err) {
+      return console.error(err);
   }
   const {port} = server.address();
   console.info(`App listening on port ${port}`);
